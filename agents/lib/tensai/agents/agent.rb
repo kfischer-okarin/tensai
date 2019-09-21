0daf614efcee31573dfcffe6c620677845ff002a
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2019-09-21 15:49:43 +0900

    Add act method

--- agents/lib/tensai/agents/agent.rb
+++ agents/lib/tensai/agents/agent.rb
@@ -5,11 +5,23 @@ require 'tensai/util/initializer'
 
 module Tensai
   module Agents
+    #
     # Agent
+    #
     class Agent
       include Util::Initializer.new(
         sensors: Types::Strict::Array
       )
+
+      #
+      # Execute one cycle of agent activity.
+      # This includes processing percepts, decision making and acting.
+      #
+      # @param context Execution context
+      #
+      def act(context) # rubocop:disable Lint/UnusedMethodArgument
+        raise NotImplementedError
+      end
     end
   end
 end
--- agents/lib/tensai/agents/sensor.rb
+++ agents/lib/tensai/agents/sensor.rb
@@ -5,20 +5,29 @@ require 'tensai/util/initializer'
 
 module Tensai
   module Agents
+    #
     # Agent sensor
+    #
     class Sensor
       include Util::Initializer.new(
         agent: Types::Any
       )
 
-      # Process input from environment.
       #
-      # The sensor should turn this input into percepts.
-      def process_input(_input)
+      # Process input from environment and produce percepts.
+      # (see #percepts)
+      #
+      # @param input Sensoric input from the environment
+      #
+      def process_input(input) # rubocop:disable Lint/UnusedMethodArgument
         raise NotImplementedError
       end
 
-      # Percepts produced from recent input
+      #
+      # Percepts produced by recent sensor input.
+      #
+      # @return [Enumerable] List of percepts
+      #
       def percepts
         raise NotImplementedError
       end
