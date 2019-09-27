936707ade4e8d506cf3eb6d61930227fc7edcca8
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2019-09-27 14:38:52 +0900

    Improve initializer util module to support inheritance

--- core/lib/tensai/util/initializer.rb
+++ core/lib/tensai/util/initializer.rb
@@ -5,42 +5,69 @@ require 'tensai/types'
 module Tensai::Util
   # @private
   class Initializer < Module
+    attr_reader :args
+
     def initialize(**args)
       @args = args
-
-      define_method :__check_argument_type do |argument, value|
-        args[argument][value]
-      end
     end
 
     def included(klass)
-      klass.class_eval initializer_code, __FILE__, __LINE__ + 10
       @args.keys.each do |arg|
         klass.class_eval "attr_reader :#{arg}", __FILE__, __LINE__
       end
+
+      all_args = all_args_for(klass)
+
+      klass.instance_eval do
+        define_method :__check_argument_type do |argument, value|
+          all_args[argument][value]
+        end
+      end
+
+      klass.class_eval InitializeMethod.new(all_args).code, __FILE__, __LINE__ + 11
     end
 
     private
 
-    def initializer_code
-      <<~CODE
-        def initialize(#{initializer_signature})
-          #{check_argument_types_code}
-          #{assign_instance_variables_code}
-        end
-      CODE
+    def all_args_for(klass)
+      parent_initializers = klass.ancestors.select { |a| a.is_a? Initializer }
+
+      parent_initializers.map(&:args)
+                         .reduce(args) { |result, parent_args| parent_args.merge result }
     end
 
-    def initializer_signature
-      @args.keys.map { |arg| "#{arg}:" }.join(', ')
-    end
+    #
+    # Generates initialize method code
+    #
+    class InitializeMethod
+      def initialize(args)
+        @args = args
+      end
 
-    def check_argument_types_code
-      @args.keys.map { |arg| "__check_argument_type(:#{arg}, #{arg})" }.join(';')
-    end
+      def code
+        <<~CODE
+          def initialize(#{signature})
+            #{check_argument_types_code}
+            #{assign_instance_variables_code}
+          end
+        CODE
+      end
 
-    def assign_instance_variables_code
-      @args.keys.map { |arg| "@#{arg} = #{arg}" }.join(';')
+      private
+
+      attr_reader :args
+
+      def signature
+        args.keys.map { |arg| "#{arg}:" }.join(', ')
+      end
+
+      def check_argument_types_code
+        args.keys.map { |arg| "__check_argument_type(:#{arg}, #{arg})" }.join(';')
+      end
+
+      def assign_instance_variables_code
+        args.keys.map { |arg| "@#{arg} = #{arg}" }.join(';')
+      end
     end
   end
 end
--- core/spec/tensai/util/initializer_spec.rb
+++ core/spec/tensai/util/initializer_spec.rb
@@ -29,6 +29,46 @@ module Tensai
       it 'does not accept arguments of wrong type' do
         expect { klass.new(arg: 4) }.to raise_error Dry::Types::CoercionError
       end
+
+      context 'When the parent class already includes an Initializer' do
+        context 'with different arguments' do
+          let(:klass) {
+            parent = Class.new
+            parent.include Initializer.new(id: Types::Strict::Integer)
+            Class.new parent
+          }
+
+          it 'accepts the right argument of the right type' do
+            instance = klass.new(arg: 'aa', id: 1)
+            expect(instance).to have_attributes(arg: 'aa', id: 1)
+          end
+
+          it 'does not accept other arguments' do
+            expect { klass.new(b: 'aa') }.to raise_error ArgumentError
+          end
+
+          it 'does not accept positional arguments' do
+            expect { klass.new('aa') }.to raise_error ArgumentError
+          end
+
+          it 'does not accept arguments of wrong type' do
+            expect { klass.new(arg: 4, id: 'aa') }.to raise_error Dry::Types::CoercionError
+          end
+        end
+
+        context 'with the same arguments' do
+          let(:klass) {
+            parent = Class.new
+            parent.include Initializer.new(arg: Types::Strict::Integer)
+            Class.new parent
+          }
+
+          it 'uses the type of the child class' do
+            instance = klass.new(arg: 'aa')
+            expect(instance).to have_attributes(arg: 'aa')
+          end
+        end
+      end
     end
   end
 end
